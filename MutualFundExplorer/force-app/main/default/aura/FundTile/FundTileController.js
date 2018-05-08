7c810d17575aae20ed5596b069af61b5c8f636eb
Skamyanau Aliaksandr <Skamyanau_AS@itd.iba.by>
2018-05-08 13:33:48 +0300

    Downloaded DreamInvest and set perms

--- MutualFundExplorer/force-app/main/default/aura/FundTile/FundTileController.js
+++
@@ -1,9 +0,0 @@
-({
-	fundSelected : function(component, event, helper) {
-		var fund = component.get("v.fund");
-		var selectSObjectEvent = $A.get("e.ltng:selectSObject");
-        selectSObjectEvent.setParams({"recordId": fund.Id});
-        selectSObjectEvent.fire();
-	},
-    
-})
